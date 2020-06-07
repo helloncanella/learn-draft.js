020570fe4edf539f24baa3becc30488d70f2421e
Hellon Canella <helloncanella@gmail.com>
2020-06-07 01:11:18 -0300

    ok

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,121 +1,123 @@
-import { addParameters, addDecorator, configure } from '@storybook/react';
-import i18n from './i18n';
-import { withI18next } from 'storybook-addon-i18next';
+import { addParameters, addDecorator, configure } from "@storybook/react"
+import i18n from "./i18n"
+import { withI18next } from "storybook-addon-i18next"
 
 addParameters({
   options: {
     isFullscreen: false,
     showPanel: true
   },
-  viewport: { // Based on this stats: https://gs.statcounter.com/screen-resolution-stats/desktop/europe
+  viewport: {
+    // Based on this stats: https://gs.statcounter.com/screen-resolution-stats/desktop/europe
     viewports: {
-      'desktop-22pc': {
-        name: '22% desktop browser market EU',
+      "desktop-22pc": {
+        name: "22% desktop browser market EU",
         styles: {
-          width: '1920px',
-          height: '1080px'
+          width: "1920px",
+          height: "1080px"
         },
-        type: 'laptop'
+        type: "laptop"
       },
-      'desktop-19pc': {
-        name: '19% desktop browser market EU',
+      "desktop-19pc": {
+        name: "19% desktop browser market EU",
         styles: {
-          width: '1366px',
-          height: '768px'
+          width: "1366px",
+          height: "768px"
         },
-        type: 'laptop'
+        type: "laptop"
       },
-      'desktop-8pc': {
-        name: '8% desktop browser market EU',
+      "desktop-8pc": {
+        name: "8% desktop browser market EU",
         styles: {
-          width: '1536px',
-          height: '864px'
+          width: "1536px",
+          height: "864px"
         },
-        type: 'laptop'
+        type: "laptop"
       },
-      'desktop-6pc': {
-        name: '6% desktop browser market EU',
+      "desktop-6pc": {
+        name: "6% desktop browser market EU",
         styles: {
-          width: '1440px',
-          height: '900px'
+          width: "1440px",
+          height: "900px"
         },
-        type: 'laptop'
+        type: "laptop"
       },
-      'desktop-5pc': {
-        name: '5% desktop browser market EU',
+      "desktop-5pc": {
+        name: "5% desktop browser market EU",
         styles: {
-          width: '1600px',
-          height: '900px'
+          width: "1600px",
+          height: "900px"
         },
-        type: 'laptop'
+        type: "laptop"
       },
-      'desktop-4pc': {
-        name: '4% desktop browser market EU',
+      "desktop-4pc": {
+        name: "4% desktop browser market EU",
         styles: {
-          width: '1280px',
-          height: '1024px'
+          width: "1280px",
+          height: "1024px"
         },
-        type: 'laptop'
+        type: "laptop"
       }, // Based on this stats: https://gs.statcounter.com/screen-resolution-stats/mobile/europe
-      'mobile-30pc': {
-        name: '30% mobile browser market EU',
+      "mobile-30pc": {
+        name: "30% mobile browser market EU",
         styles: {
-          width: '360px',
-          height: '640px'
+          width: "360px",
+          height: "640px"
         },
-        type: 'mobile'
+        type: "mobile"
       },
-      'mobile-11pc': {
-        name: '11% mobile browser market EU',
+      "mobile-11pc": {
+        name: "11% mobile browser market EU",
         styles: {
-          width: '375px',
-          height: '667px'
+          width: "375px",
+          height: "667px"
         },
-        type: 'mobile'
+        type: "mobile"
       },
-      'mobile-6pc': {
-        name: '6% mobile browser market EU',
+      "mobile-6pc": {
+        name: "6% mobile browser market EU",
         styles: {
-          width: '360px',
-          height: '720px'
+          width: "360px",
+          height: "720px"
         },
-        type: 'mobile'
+        type: "mobile"
       },
-      'mobile-5pc': {
-        name: '5% mobile browser market EU',
+      "mobile-5pc": {
+        name: "5% mobile browser market EU",
         styles: {
-          width: '412px',
-          height: '846px'
+          width: "412px",
+          height: "846px"
         },
-        type: 'mobile'
+        type: "mobile"
       },
-      'mobile-3pc': {
-        name: '3% mobile browser market EU',
+      "mobile-3pc": {
+        name: "3% mobile browser market EU",
         styles: {
-          width: '360px',
-          height: '760px'
+          width: "360px",
+          height: "760px"
         },
-        type: 'mobile'
+        type: "mobile"
       }
     }
   }
-});
+})
 
 function loadStories(r) {
-    r.keys().sort().forEach(filename => r(filename));
+  r.keys()
+    .sort()
+    .forEach(filename => r(filename))
 }
 
 addDecorator(
   withI18next({
     i18n,
     languages: {
-      en: 'English',
-      fr: 'French'
+      en: "English",
+      fr: "French"
     }
   })
-);
+)
 
 configure(() => {
-    loadStories(require.context('../stories/components', true, /\.js$/));
-    loadStories(require.context('../stories/pages', true, /\.js$/));
-}, module);
+  loadStories(require.context("../stories", true, /\.js$/))
+}, module)
